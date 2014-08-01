c59ed63f940de5e7b36b14c1ffe887fc803cb0b3
Xolani Mfuko & Sterling Houghton <student@epicodus.com>
2014-08-01 11:19:21 -0700

    Completed the task object and its tests

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -3,6 +3,10 @@ require 'list'
 require 'task'
 
 describe Task do
+  before do
+    Task.clear
+  end
+
   it "initializes an object that is an instance of the class" do
     test_task = Task.new("Learn the ruby object model")
     test_task.should be_an_instance_of Task
@@ -11,5 +15,23 @@ describe Task do
     test_task = Task.new("Learn the ruby object model")
     test_task.name.should eq "Learn the ruby object model"
   end
-end
 
+  describe ".all" do
+    it "starts with an empty array" do
+      Task.all.should eq []
+    end
+    it "gets added to array when save is called on an instance" do
+      test_task = Task.new("Learn the ruby object model")
+      test_task.save
+      Task.all.should eq [test_task]
+    end
+  end
+
+  describe ".clear" do
+    it "empties out all the saved tasks" do
+      Task.new("Learn the ruby object model").save
+      Task.clear
+      Task.all.should eq []
+    end
+  end
+end
